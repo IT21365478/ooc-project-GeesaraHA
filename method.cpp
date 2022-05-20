31f4d31e59a901141cc85b6059f72fa4fd93110f
IT21365478 <105938015+IT21365478@users.noreply.github.com>
2022-05-20 22:30:38 +0530

    Create method.cpp

---
+++ method.cpp
@@ -0,0 +1,47 @@
+void User::setUserDetails(string Email,string Uname, string Pword)
+{
+ Email=Email;
+ Username=Uname;
+ Password=Pword;
+}
+void User::registerUser()
+{
+ cout<<"email="<<Email<<endl
+     <<"Username"<<Username<<endl
+     <<"password"<<Password<<endl<<endl;
+}
+
+void App::setAppDetails(int AID,string Aname, string Atype)
+{
+ AppID=AID;
+ Appname=Aname;
+ Apptype=Atype;
+}
+void App::displayApp()
+{
+ cout<<"Appid="<<AppID<<endl
+     <<"Appname"<<Appname<<endl
+     <<"Apptype"<<Apptype<<endl<<endl;
+}
+
+void Payment::setPaymentDetails(int PID,string Ptype)
+{
+ paymentID=PID;
+ paymentType=Ptype;
+}
+void payment::Payment()
+{
+ cout<<"PaymentID="<<paymentID<<endl
+     <<"PaymentType"<<paymentType<<endl<<endll;
+}
+
+void Download::setDownloadDetails(int DID,string Dtype)
+{
+ downloadID=DID;
+ downloadType=Dtype;
+}
+void Download::Download()
+{
+ cout<<"DownlaodID="<<downloadID<<endl
+     <<"DownloadType"<<DownlaodType<<endl<<endll;
+}
